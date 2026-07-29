42dcd22cf6c3fd7568dca874e9e31c50aff44848
sush1110 <sushmagotru@gmail.com>
2026-07-29 08:57:36 +0000

    html

---
+++ js/script.js
@@ -0,0 +1,13 @@
+alert("Welcome to NRIIT Learning Management System")
+let heading = document.getElementById("welcome");
+heading.innerHTML = "Welcome Future Software Engineers"
+console.log("Heading element: ", heading)
+let msg = document.getElementById("message")
+msg.innerHTML = "Javascript is fun"
+console.Log("Message element:", msg)
+function showmessage() {
+    alert("Welcome to NRIIT Learning Management System")
+}
+function changeHeading() {
+    document.getElementById("Welcome").innerHTML = "Welcome Python Fullstack Developers"
+}
