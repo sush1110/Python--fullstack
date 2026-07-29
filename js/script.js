6fc87702bd421b6bb7d2b6d73630be0f5b4c36b6
sush1110 <sushmagotru@gmail.com>
2026-07-29 10:49:42 +0000

    eventlistners

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 alert("Welcome to NRIIT Learning Management System")
 let heading = document.getElementById("welcome");
-heading.innerHTML = "Welcome Future Software Engineers"
+heading.innerHTML = "Welcome Future software engineers"
 console.log("Heading element: ", heading)
 let msg = document.getElementById("message")
 msg.innerHTML = "Javascript is fun"
@@ -11,3 +11,21 @@ function showmessage() {
 function changeHeading() {
     document.getElementById("Welcome").innerHTML = "Welcome Python Fullstack Developers"
 }
+let heading1=document.querySelection("#welcome");
+console.log("Heading element:",heading1)
+let button = document.getElementById("btnGreeting");
+button.addEventListener("click", function() {
+    alert("Welcome to javascript Event Handling");
+});
+let registerForm = document.getElementById("registerForm");
+function (event) {
+    event.preventDefault(); // prevent form submision
+    let name = document.getElementById("name").Value;
+    let email = document.getElementById("email").Value;
+    let password = document.getElementById("password").Value;
+    if (!name || !email || !password){
+        alert("please fill in all iels.:");
+        return; 
+    }
+    alert("Registration succesfull");
+    //perform
